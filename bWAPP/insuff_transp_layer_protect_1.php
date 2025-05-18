07579dd4e4ac52e1c1066124f68aeb9bf3bd1326
cod3mau5 <admin@codemau5.com>
2025-05-17 21:29:20 -0700

    v2.2

--- bWAPP/insuff_transp_layer_protect_1.php
+++ bWAPP/insuff_transp_layer_protect_1.php
@@ -5,20 +5,68 @@
 bWAPP, or a buggy web application, is a free and open source deliberately insecure web application.
 It helps security enthusiasts, developers and students to discover and to prevent web vulnerabilities.
 bWAPP covers all major known web vulnerabilities, including all risks from the OWASP Top 10 project!
-It is for educational purposes only.
+It is for security-testing and educational purposes only.
 
 Enjoy!
 
 Malik Mesellem
 Twitter: @MME_IT
 
-© 2014 MME BVBA. All rights reserved.
+bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (http://creativecommons.org/licenses/by-nc-nd/4.0/). Copyright © 2014 MME BVBA. All rights reserved.
 
 */
 
 include("security.php");
 include("security_level_check.php");
 include("selections.php");
+
+$message = "";
+
+switch($_COOKIE["security_level"])
+{
+
+    case "0" :
+
+        $url = $_SERVER["SCRIPT_NAME"];
+        break;
+
+    case "1" :
+
+        $message = "A certificate must be configured to fully function!";
+        $url = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
+        break;
+
+    case "2" :
+
+        $message = "A certificate must be configured to fully function!";
+        $url = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
+        break;
+
+    default :
+
+        $url = $_SERVER["SCRIPT_NAME"];
+        break;
+
+}
+
+if(isset($_POST["form"]))   
+{
+
+    if($_POST["login"] == $login && $_POST["password"] == $password)
+    {
+
+        $message = "<font color=\"green\">Successful login!</font>";
+
+    }
+
+    else
+    {
+
+        $message = "<font color=\"red\">Invalid credentials!</font>";
+
+    }
+
+}
 
 ?>
 <!DOCTYPE html>
@@ -28,14 +76,14 @@ include("selections.php");
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter">
+<!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter">-->
 <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen" />
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
 
 <!--<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
 <script src="js/html5.js"></script>
 
-<title>bWAPP - Security Misconfiguration</title>
+<title>bWAPP - Insufficient Transport Layer Protection</title>
 
 </head>
 
@@ -47,7 +95,7 @@ include("selections.php");
 
 <h2>an extremely buggy web app !</h2>
 
-</header>    
+</header>
 
 <div id="menu">
 
@@ -59,7 +107,7 @@ include("selections.php");
             <td><a href="password_change.php">Change Password</a></td>
             <td><a href="user_extra.php">Create User</a></td>
             <td><a href="security_level_set.php">Set Security Level</a></td>
-            <td><a href="reset.php" onclick="return confirm('All settings will be cleared. Are you sure?');">Reset</a></td>            
+            <td><a href="reset.php" onclick="return confirm('All settings will be cleared. Are you sure?');">Reset</a></td>
             <td><a href="credits.php">Credits</a></td>
             <td><a href="http://itsecgames.blogspot.com" target="_blank">Blog</a></td>
             <td><a href="logout.php" onclick="return confirm('Are you sure you want to leave?');">Logout</a></td>
@@ -67,32 +115,45 @@ include("selections.php");
 
         </tr>
 
-    </table>   
-
-</div> 
-
-<div id="main">
-
-    <h1>Local Privilege Escalation (udev)</h1>
-
-    <p>The Linux kernel is vulnerable to a local privilege attack. G3t r00t! (<a href="http://sourceforge.net/projects/bwapp/files/bee-box/" target="_blank">bee-box</a> only)</p>
-    
-    <p>HINT: a public <a href="../evil/cve-2009-1185.c" target="_blank">exploit</a> is available!
+    </table>
 
 </div>
 
-<div id="side">    
+<div id="main">
 
-    <a href="http://itsecgames.blogspot.com" target="blank_" class="button"><img src="./images/blogger.png"></a>
-    <a href="http://be.linkedin.com/in/malikmesellem" target="blank_" class="button"><img src="./images/linkedin.png"></a>
+    <h1>Clear Text HTTP (Credentials)</h1>
+
+    <p>Enter your credentials <i>(bee/bug)</i>.</p>
+
+    <form action="<?php echo $url?>" method="POST">
+
+        <p><label for="login">Login:</label><br />
+        <input type="text" id="login" name="login" size="20" /></p>
+
+        <p><label for="password">Password:</label><br />
+        <input type="password" id="password" name="password" size="20" /></p>
+
+        <button type="submit" name="form" value="submit">Login</button>
+
+    </form>
+
+    </br >
+    <?php echo $message;?>
+
+</div>
+
+<div id="side">
+
     <a href="http://twitter.com/MME_IT" target="blank_" class="button"><img src="./images/twitter.png"></a>
+    <a href="http://be.linkedin.com/in/malikmesellem" target="blank_" class="button"><img src="./images/linkedin.png"></a>
     <a href="http://www.facebook.com/pages/MME-IT-Audits-Security/104153019664877" target="blank_" class="button"><img src="./images/facebook.png"></a>
+    <a href="http://itsecgames.blogspot.com" target="blank_" class="button"><img src="./images/blogger.png"></a>
 
-</div>     
+</div>
 
 <div id="disclaimer">
 
-    <p>bWAPP is for educational purposes only / Follow <a href="http://twitter.com/MME_IT" target="_blank">@MME_IT</a> on Twitter and ask for our cheat sheet, containing all solutions! / Need a <a href="http://www.mmeit.be/bWAPP/training.htm" target="_blank">training</a>? / &copy; 2014 MME BVBA</p>
+    <p>bWAPP is licensed under <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img style="vertical-align:middle" src="./images/cc.png"></a> &copy; 2014 MME BVBA / Follow <a href="http://twitter.com/MME_IT" target="_blank">@MME_IT</a> on Twitter and ask for our cheat sheet, containing all solutions! / Need an exclusive <a href="http://www.mmebvba.com" target="_blank">training</a>?</p>
 
 </div>
 
@@ -104,7 +165,7 @@ include("selections.php");
 
 <div id="security_level">
 
-    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
+    <form action="<?php echo ($_SERVER["SCRIPT_NAME"]);?>" method="POST">
 
         <label>Set your security level:</label><br />
 
@@ -112,14 +173,14 @@ include("selections.php");
 
             <option value="0">low</option>
             <option value="1">medium</option>
-            <option value="2">high</option> 
+            <option value="2">high</option>
 
         </select>
 
         <button type="submit" name="form_security_level" value="submit">Set</button>
         <font size="4">Current: <b><?php echo $security_level?></b></font>
 
-    </form>   
+    </form>
 
 </div>
 
